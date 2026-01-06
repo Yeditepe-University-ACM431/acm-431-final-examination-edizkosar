d15d4d5dd8d337169d83495c5b0bb77a6644606b
edizkosar <ediztwitch@gmail.com>
2026-01-06 14:56:26 +0300

    Update TaskViewModel.kt

--- app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
@@ -6,7 +6,10 @@ class TaskViewModel : ViewModel() {
 
     // TODO 1: Create a mutable state list of Task objects
     // Initially add at least 2 tasks
-
+    private val _tasks = mutableStateListOf(
+    Task(1, "Task 1", false),
+    Task(2, "Task 2", true)
+)
     fun toggleTask(taskId: Int) {
         // TODO 2: Update isCompleted for the given task
     }
